652f67ef27c767d2d72b77f77d3538b52cbbd6e6
posholokia <ilya.posholokk@gmail.com>
2024-08-31 15:34:57 +0600

    fix: webhook log

--- src/api/urls.py
+++ src/api/urls.py
@@ -43,39 +43,40 @@ def healthcheck(request: HttpRequest) -> None:
 def get_webhooks(request: HttpRequest) -> HttpResponse:
     path = settings.BASE_DIR
     line_list = []
-    with open(f"{path}/logs/warnings.log") as file:
+    with open(f"{path}/log/warnings.log") as file:
         lines = file.readlines()[-10:]
         for line in lines:
             time, data = line.split("WARNING CMS WEBHOOK: body: b'")
             data = data.split("', header:")[0].rstrip(",\n").rstrip("'\n")
             line_list.append((time, json.loads(data.encode())))
 
-    html = [dict_render(obj[1]) for obj in line_list]
+    html = [dict_render(time_log=obj[0], data=obj[1]) for obj in line_list]
     return render(
         request,
         "index.html",
-        {"data": [(time, obj) for obj in html]}
+        {"data": [obj for obj in html]}
     )
 
 
-def dict_render(data: dict[str, str | dict[str, str]], deep=0) -> str:
+def dict_render(data: dict[str, str | dict[str, str]],  time_log="", deep=0) -> str:
     tab = " " * 4
     res = "{\n"
     deep += 1
+    time_log_line = f"<strong>{time_log}</strong>"
     for key, value in data.items():
         res += tab * deep + f"{key}:  "
 
         if isinstance(value, dict):
-            res += dict_render(value, deep) + ",\n"
+            res += dict_render(value, deep=deep) + ",\n"
         elif isinstance(value, list):
             res += "["
             for item in value:
-                res += "\n" + (tab * (deep+1)) + (dict_render(item, deep+1) + ",\n" + tab * deep)
+                res += "\n" + (tab * (deep+1)) + (dict_render(item, deep=deep+1) + ",\n" + tab * deep)
             res += "],\n"
         else:
             res += str(value) + ",\n"
     res += tab * (deep - 1) + "}"
-    return res
+    return time_log_line + res
 
 
 api.add_router("v1/", v1_router)
